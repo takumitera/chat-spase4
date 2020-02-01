ec40961afb5425eec9cc961245d49496a7f2d818
takumitera <takumi.terakawa@gmail.com>
2020-02-01 19:52:52 +0900

    練習1

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
-
-def hello
-  p "hello, GitHub Desk top."
-end
-
-hello
